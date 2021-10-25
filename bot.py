819116284f3848675b09821190701c627c8ef619
xxjhonxxvale <jhonx100pre1999@hotmail.com>
2021-10-25 18:20:24 -0500

    Actualizacion 2 del bot

--- bot.py
+++ bot.py
@@ -1,37 +1,37 @@
+import os
 
+os.environ['token']
 from telegram import Update
-from telegram.ext import dispatcher, Updater, CommandHandler
+from telegram.ext import dispatcher, Updater, CommandHandler, MessageHandler, Filters
 
-# def start(udpate, context):
+# Configuramos el comando start para enviar un mensaje de bienvenida
 def start(update, context):
 
     update.message.reply_text('Hola humano!')
 
+def echo(update, context):
+    # Configuramos para enviar un mensaje con el texto que envió el usuario, aquí es donde va a ir toda la lógica de nuestro bot
+    # Es decir... la instruccion que hace que se repita todo lo que envie de mensaje que no sea un comando
+    update.message.reply_text(update.message.text)
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__' :  #significa que el programa iniciara a partir de aqui
     
-    updater = Updater(token='TuToken', use_context=True)
+    # Creamos el Updater y le pasamos el token de nuestro bot. Este se encargará de manejar las peticiones de los usuarios.
+    # El 'udpater' es una variable
+    updater = Updater(token='token', use_context=True)
 
+    # Obtenemos el Dispatcher para crear los comandos
     dp = updater.dispatcher
     
+    # Creamos el comando /start y definimos que se ejecute este mismo método
     dp.add_handler(CommandHandler('start', start))
+
+    # De no ejecutarse ninguno de los anteriores asumimos que el usuario escribió algo y ejecutamos el método echo que nos va a permitir obtener los campos de las búsquedas del usuario
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
     updater.idle()
 
-# from telegram import Update
-# from telegram.ext import Updater, CommandHandler, CallbackContext
-
-
-# def hola(update: Update, context: CallbackContext) -> None:
-#     update.message.reply_text(f'Kenny afina pistolas {update.effective_user.first_name}')
-
-
-# updater = Updater('2097638862:AAEmflVzqB8L6ux6r02hDm10TMhpkVNSTVs', use_context=True)
-
-# updater.dispatcher.add_handler(CommandHandler('hola', hola))
-
-# updater.start_polling()
-# updater.idle()
-
+#1 Se declaran los comandos dentro del __Main__
+#2 Se declaran las accioden del comando antes del __Main__
