8d7d2f2a004a3e433f1255ec401fbe0a475a44c8
xxjhonxxvale <jhonx100pre1999@hotmail.com>
2021-10-25 23:02:09 -0500

    Correcion de _ al bot2

--- bot2.py
+++ bot2.py
@@ -3,10 +3,10 @@ import telegram
 from telegram.ext import Updater, CommandHandler, updater
 
 def Start(update, context):
-    update.message.reply.text("Hola, otra vez :')")
+    update.message.reply_text("Hola, otra vez :')")
 
 def Help(update, context):
-    update.message.reply.text('Usa /qr para generar un codigo qr')
+    update.message.reply_text('Usa /qr para generar un codigo qr')
 
 if __name__ == '__main__':
 
