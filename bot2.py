efe5eebc6ba7c2687e61b33604c84852cf699236
xxjhonxxvale <jhonx100pre1999@hotmail.com>
2021-10-25 23:20:11 -0500

    se agrego el comando qr

--- bot2.py
+++ bot2.py
@@ -1,13 +1,16 @@
 import os
 import telegram
 from telegram import Update, bot
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, ConversationHandler
 
 def start(update, context):
     update.message.reply_text("Hola, otra vez :')")
 
 def help(update, context):
     update.message.reply_text('Usa /qr para generar un codigo qr')
+
+def qr_command_handler(update, context):
+    update.message.reply_text('Inviame el texto para generar el qr')
 
 if __name__ == '__main__':
 
@@ -22,6 +25,17 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('Start', start))
 
     dp.add_handler(CommandHandler('Help', help))
+
+    dp.add_handler(ConversationHandler(
+        entry_points=[
+            ConversationHandler('qr', qr_command_handler)
+        ]
+
+        states={}
+
+        fallbacks=[]
+
+    ))
 
 
     updater.start_polling()
